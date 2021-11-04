cdb4aaeb5bdc4a1ead370420bfa4e34f3fd8406d
Javier Godoy <javiere.godoyc@gmail.com>
2021-11-04 19:03:12 -0300

    api terminada

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -10,7 +10,50 @@ def ping():
 
 @app.route("/products")
 def getProducts():
-    return jsonify({"products": products, "message":})
+    return jsonify({"products": products, "message": "Product's List"})
+
+@app.route("/products/<string:product_name>")
+def getProduct(product_name):
+    productFound = [product for product in products if product['name'] == product_name] 
+    if len(productFound) > 0:
+        return jsonify({"product": productFound[0]})
+    else:
+        return jsonify({"message": "Product not found"})
+
+@app.route('/products', methods=['POST'])
+def addProduct():
+    new_product = {
+        "name": request.json['name'],
+        "price": request.json['price'],
+        "quantity": request.json['quantity']
+    }
+    products.append(new_product)
+    return jsonify({'messagge': "Product Added Succesfully", "products": products})
+
+@app.route('/products/<string:product_name>', methods=["PUT"])
+def editProduct(product_name):
+    productFound = [product for product in products if product['name'] == product_name]
+    if(len(productFound)> 0):
+        productFound[0]['name'] = request.json['name']
+        productFound[0]['price'] = request.json['price']
+        productFound[0]['quantity'] = request.json['quantity']
+        return jsonify({
+            "message": "Product Updated",
+            "product": productFound[0]
+        })
+    else:
+        return jsonify({"message": "Product not Found"})
+
+@app.route('/products/<string:product_name>', methods=["DELETE"])
+def deleteProduct(product_name):
+    productFound = [product for product in products if product['name'] == product_name]
+    if len(productFound) > 0:
+        products.remove(productFound[0])
+        return jsonify({
+            "message": "Product Deleted",
+            "products": products
+        })
+    return jsonify({"message": "Product Not Found"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
